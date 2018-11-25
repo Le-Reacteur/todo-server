69dcdb76e497cfb3474f0365500377f877058e27
Farid Safi <faridsafi@noreply.github.com>
2018-11-25 18:59:57 +0100

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const Task = mongoose.model("task", {
   title: String
