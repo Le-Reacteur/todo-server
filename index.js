3c17b42dc53df92e45375d9a74bfabb734ac71d5
Farid Safi <faridsafi@noreply.github.com>
2018-11-25 18:47:14 +0100

    Add Task model and routes for READ and CREATE

--- index.js
+++ index.js
@@ -5,13 +5,32 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.json());
 
+const Task = mongoose.model("task", {
+  title: String
+});
+
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/todo-app",
   { useNewUrlParser: true }
 );
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  Task.find().exec((err, tasks) => {
+    if (err) {
+      return res.status(400).json({ error: "An error occurred" });
+    }
+    return res.json(tasks);
+  });
+});
+
+app.post("/create", (req, res) => {
+  const task = new Task({ title: req.body.title });
+  task.save(err => {
+    if (err) {
+      return res.status(400).json({ error: "An error occurred" });
+    }
+    return res.json(task);
+  });
 });
 
 app.listen(process.env.PORT || 3100, () => {
